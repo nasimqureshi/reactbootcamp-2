0f571fc440c5532134cfa2abdf4e68ead8e013b8
nasimqureshi <qureshi.nasim@yahoo.com>
2021-02-03 10:46:53 -0600

    fifth commit

--- src/index.js
+++ src/index.js
@@ -18,19 +18,33 @@ const secondBook ={
 function BookList(){
   return(
     <section className='list'>
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
+    <Book
+      img={firstBook.img} 
+      title={firstBook.title}
+      author={firstBook.author} 
+      >
+      <p>Excellent course for beginners. The course material is neat & crisp with adequate examples to support the concepts.
+       The final exercises are designed in such a way to put every concept learnt across the course to put in practice.
+       Instructor explains the concepts at a constant pace along with quite good amount of additional information.
+       Overall, very much satisfied with this learning experience.</p>
+
+      </Book>
+    <Book 
+      img={secondBook.img}
+      title={secondBook.title} 
+      author={secondBook.author} />
       </section>
   )
 }
 
-const Book = (props) => {
-  const { img, title, author } = props;
+const Book = ({img, title, author, children}) => {
+  //const { img, title, author } = props;
   return(
     <article className='book'>
       <img src={img} alt ="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      {children}
     </article>
   );
   };
