c16b69cc047b6b75e9e5723e2094a97759828a65
nasimqureshi <qureshi.nasim@yahoo.com>
2021-02-03 08:16:06 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -2,36 +2,34 @@ import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import './index.css';
 
+// Variables 
+const title ='The Odyssey';
+const author ='Stieg Larson';
+const img =
+'https://img.thriftbooks.com/api/images/s/fedfc44765ea0090f3f942ce3865c097f6b4f578.jpg';
+
 function BookList(){
+  
   return(
     <section className='list'>
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </section>
+      </section>
   )
 }
-const Book = () => {
+
+const Book = (props) => {
   return(
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt ="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
-};
+  };
 
 
-const Image = () => (
-  <img src="https://img.thriftbooks.com/api/images/s/fedfc44765ea0090f3f942ce3865c097f6b4f578.jpg" alt=""></img>
-);
-const Title = () =><h1>The Odyssey</h1>;
-const Author = () => <h4 style={{color:'#617d98',fontSize:'0.75rem',
-marginTop: '0.25rem'}}>Stieg Larson</h4>
+
+
 
 ReactDOM.render(<BookList />, document.getElementById('root')
 );
