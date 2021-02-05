4f1a8cad976e9c8fdc8af79f311f1c726a709aea
nasimqureshi <qureeshi.nasim@yahoo.com>
2021-02-05 12:13:07 -0600

    eighth commit

--- src/index.js
+++ src/index.js
@@ -3,36 +3,25 @@ import ReactDOM, { render } from 'react-dom';
 import './index.css';
 
 // Variables 
-const firstBook ={
+const books [
+{
   img: 'https://img.thriftbooks.com/api/images/s/fedfc44765ea0090f3f942ce3865c097f6b4f578.jpg',
   title:'The Odyssey',
   author: 'Stieg Larson'
 }
-const secondBook ={
+,
+{
   img: 'https://images-na.ssl-images-amazon.com/images/I/51b-OAdpJqL._AC_SX184_.jpg',
   title:'How Toddlers Thrive',
   author: 'Tovah P Klein'
 }
-
+,
+]
 
 function BookList(){
   return(
-    <section className='booklist'>
-    <Book
-      img={firstBook.img} 
-      title={firstBook.title}
-      author={firstBook.author} 
-      >
-      <p>Excellent course for beginners. The course material is neat & crisp with adequate examples to support the concepts.
-       The final exercises are designed in such a way to put every concept learnt across the course to put in practice.
-       Instructor explains the concepts at a constant pace along with quite good amount of additional information.
-       Overall, very much satisfied with this learning experience.</p>
-
-      </Book>
-    <Book 
-      img={secondBook.img}
-      title={secondBook.title} 
-      author={secondBook.author} />
+    <section className='Booklist'>
+    
       </section>
   )
 }
